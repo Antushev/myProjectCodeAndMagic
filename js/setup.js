297eb68d190ce6d1802eaf2109a4760f89e2082f
DimAntushev <fyneitd@mail.ru>
2020-03-01 18:37:08 +0300

    Разделение проекта по модулям, перетаскивание

--- js/setup.js
+++ js/setup.js
@@ -41,5 +41,53 @@
       closeSetup();
     }
   });
+
+  var pictureUserInSetup = setupWindow.querySelector('.upload');
+
+  pictureUserInSetup.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+    var dragged = false;
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      setupWindow.style.left = (setupWindow.offsetLeft - shift.x) + 'px';
+      setupWindow.style.top = (setupWindow.offsetTop - shift.y) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      pictureUserInSetup.removeEventListener('mousemove', onMouseMove);
+      pictureUserInSetup.removeEventListener('mouseup', onMouseUp);
+
+      if (dragged) {
+        var onClickPreventDefault = function (clickEvt) {
+          clickEvt.preventDefault();
+          setupWindow.removeEventListener('click', onClickPreventDefault)
+        };
+        setupWindow.addEventListener('click', onClickPreventDefault);
+      }
+    };
+
+    pictureUserInSetup.addEventListener('mousemove', onMouseMove);
+    pictureUserInSetup.addEventListener('mouseup', onMouseUp);
+  });
 })();
 
